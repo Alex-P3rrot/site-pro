242fffc8bc4191d678b210cb6b4f3ef6cd059a31
perrot.alex.dev <perrot.alex.dev@gmail.com>
2023-06-29 10:20:28 +0200

    Add logo

--- assets/react/controllers/SectionProjects.jsx
+++ assets/react/controllers/SectionProjects.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import {Project} from "./Project"
 
 const LogoOhani = require('../../images/Web_vert.png')
 
 export function SectionProjects() {
     return (
-        <Box width="100%" display="grid" gridTemplateColumns="repeat(2, 1fr)" sx={{px:2}}>
-            <Project title="Ohani" description="E-commerce d'un magasin de vêtements pour femme à Tahiti" src={LogoOhani} stack={['PHP8', 'Symfony6', 'JavaScript', 'Vue.js']}/>
+        <Box id="sectionProject">
+            <Typography textAlign="center" paddingY={15} variant="h3">Mes réalisations</Typography>
+            <Box width="100%" display="grid" gridTemplateColumns="repeat(2, 1fr)" sx={{px: 2}}>
+                <Project title="Ohani" description="E-commerce d'un magasin de vêtements pour femme à Tahiti"
+                         src={LogoOhani} stack={['PHP8', 'Symfony6', 'JavaScript', 'Vue.js']}/>
+            </Box>
         </Box>
     )
 }
--- assets/react/controllers/Topbar.jsx
+++ assets/react/controllers/Topbar.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import {Box, IconButton, useTheme} from "@mui/material";
+import {Box, IconButton, Link, Tooltip, Typography, useTheme} from "@mui/material";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import {ColorModeContext} from "../theme";
 import {useContext} from "react";
+import CodeIcon from '@mui/icons-material/Code';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+const logoModeDark = require('../../images/logoModeDark.png')
+const logoModeLight = require('../../images/logoModeLight.png')
 
 export function Topbar() {
     const theme = useTheme()
     const colorMode = useContext(ColorModeContext)
+    const toProjectSection = () => {
+        const sectionProject =  document.getElementById('sectionProject')
+        console.log(sectionProject)
+        sectionProject.scrollIntoView({behavior: "smooth"})
+    }
 
     return (
         <Box display="flex" justifyContent="space-between" p={2}>
             <Box>
-                {/*<img src={logo} height={50} alt="logo"/>*/}
+                <img src={theme.palette.mode === "dark" ? logoModeDark : logoModeLight} height={100} alt="logo"/>
             </Box>
-            <IconButton onClick={colorMode.toggleColorMode}>
-                {theme.palette.mode === 'dark' ?
-                    (<LightModeIcon></LightModeIcon>)
-                    :
-                    (<DarkModeIcon></DarkModeIcon>)
-                }
-            </IconButton>
+            <Box>
+                <Tooltip title={<Typography>Réalisations</Typography>}>
+                    <IconButton onClick={toProjectSection}>
+                        <CodeIcon/>
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title={<Typography>Github</Typography>}>
+                    <IconButton onClick={() => window.open('https://github.com/Alex-P3rrot', '_blank')}>
+                        <GitHubIcon/>
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title={<Typography>Linkedin</Typography>}>
+                    <IconButton onClick={() => window.open('https://www.linkedin.com/in/alex-perrot/', '_blank')}>
+                        <LinkedInIcon/>
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title={<Typography>Mode {theme.palette.mode === "dark" ? 'light' : 'dark'}</Typography>}>
+                    <IconButton onClick={colorMode.toggleColorMode}>
+                        {theme.palette.mode === 'dark' ?
+                            (<LightModeIcon></LightModeIcon>)
+                            :
+                            (<DarkModeIcon></DarkModeIcon>)
+                        }
+                    </IconButton>
+                </Tooltip>
+            </Box>
         </Box>
     )
 }
